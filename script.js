43b51fd030e513e1360e3a32061e120750a4408e
Cataflon <catalinfrentiu1@gmail.com>
2022-10-15 11:52:33 +0300

    completare proiect

--- script.js
+++ script.js
@@ -4,18 +4,55 @@ const parola = document.querySelector(".pass");
 const email = document.querySelector(".email");
 const phone = document.querySelector(".phone");
 const error = document.querySelector(".eroare");
-
+let j;
+let flag=0;
 button.addEventListener("click",()=>{
     if(nume.value===""){
      error.style.visibility="visible";
      error.innerHTML = "Invalid user name";
+     flag=1;
      setTimeout(()=>{
         error.style.visibility="hidden";
-     },1000)
-    }
+     },1000)   
+}
+if(parola.value.length<8){
+   error.style.visibility="visible";
+   error.innerHTML = "Password needs to be at least 8 characters long";
+   flag=1;
+   setTimeout(()=>{
+      error.style.visibility="hidden";
+   },1000)   
+}
+if(!email.value.includes("@")){
+   error.style.visibility="visible";
+   error.innerHTML = "Invalid email address";
+   flag=1;
+   setTimeout(()=>{
+      error.style.visibility="hidden";
+   },1000)
+}
+
+if(typeof(+phone.value) !== NaN && phone.value.lenght >= 10 ){
+   error.style.visibility="visible";
+   error.innerHTML = "Invalid number";
+   flag=1;
+   setTimeout(()=>{
+      error.style.visibility="hidden";
+   },1000)
+}
+if(flag===0 ){
+   error.style.visibility="visible";
+   error.innerHTML = "Form completed succesfully";
+   setTimeout(()=>{
+      error.style.visibility="hidden";
+   },1000)
+}
 })
 
-//Password - lungime de 8 caractere minime
-//Email - sa contina @
-//Phone number - lungime de numar valid (10 numere)
-//CUM SA LE FAC? google manipulare string-uri js.
+
+// phone.value.split("").forEach(character => {
+//    if(!(+character >= 0 && +character <= 9)){
+//       flag = 1;
+//    }
+// });
+
